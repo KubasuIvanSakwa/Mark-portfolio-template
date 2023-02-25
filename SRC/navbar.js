30ec419ff951440725371b8de46c244ae270e333
Kubasu Ivan Sakwa <72573043+KubasuIvanSakwa@users.noreply.github.com>
2023-02-25 21:29:09 +0300

    Add files via upload
    linked the navbar better to there pages
    sorted the image in main page

--- SRC/navbar.js
+++ SRC/navbar.js
@@ -1,5 +1,5 @@
 const navbar = document.getElementById("navbar");
-fetch("SRC/navB.html")
+fetch("SRC/navB.htm")
   .then((response) => response.text())
   .then((html) => (navbar.innerHTML = html))
 
